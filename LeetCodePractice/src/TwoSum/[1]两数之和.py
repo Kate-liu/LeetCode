8b1333250c27b931541d08064ea2715836ac6380
rmliu <2218207429@qq.com>
2020-08-19 21:48:31 +0800

    Add python answer of the Two Sum.

---
+++ LeetCodePractice/src/TwoSum/[1]两数之和.py
@@ -0,0 +1,46 @@
+# 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。 
+# 
+#  你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。 
+# 
+#  
+# 
+#  示例: 
+# 
+#  给定 nums = [2, 7, 11, 15], target = 9
+# 
+# 因为 nums[0] + nums[1] = 2 + 7 = 9
+# 所以返回 [0, 1]
+#  
+#  Related Topics 数组 哈希表 
+#  👍 8938 👎 0
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        Time Complexity is O(n).
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        if len(nums) <= 1:
+            return False
+
+        dic = {}
+
+        for i in range(len(nums)):
+            if nums[i] in dic:
+                return [dic[nums[i]], i]
+            else:
+                dic[target - nums[i]] = i
+
+        # another way
+        # dic = {}
+        # for i, n in enumerate(nums):
+        #     m = target - n
+        #     if m in dic:
+        #         return [dic[m], i]
+        #     else:
+        #         dic[n] = i
+# leetcode submit region end(Prohibit modification and deletion)
--- README.md
+++ README.md
@@ -81,6 +81,7 @@ My LeetCode link-cn: https://leetcode-cn.com/u/rmliu/
 - Two Sum (两数之和)
     - LeetCode: [Two Sum](https://leetcode-cn.com/problems/two-sum/)
     - localAnswer-java: [Two Sum](./LeetCodePractice/src/TwoSum/[1]两数之和.java)
+    - localAnswer-python: [Two Sum](./LeetCodePractice/src/TwoSum/[1]两数之和.py)
 
 - Three Sum (三数之和)
     - LeetCode: [Three Sum](https://leetcode-cn.com/problems/container-with-most-water/)
